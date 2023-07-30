cad9c7b9a2cf28edad7e4c910e19e5413b1f76cf
RokaiDeveloper <140258125+RokaiDeveloper@users.noreply.github.com>
2023-07-30 09:30:58 -0300

    ajustes para funcionamento do debug
    correção de escrita para correção da ferramenta de debug

--- projeto/urls.py
+++ projeto/urls.py
@@ -14,11 +14,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from django.contrib.auth import views as auth_views
 from app.views import home, painel, dologin, dashboard, logouts, create, store, map, perfil
 urlpatterns = [
-    path("__debug__/", include("debug_toolbar.urls")),
+    path('__debug__/', include('debug_toolbar.urls')),
     path('admin/', admin.site.urls),
     path('', home),
     path('home/', home),
